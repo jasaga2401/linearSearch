e09d86ba4f92ff002ac38e94f1e31ee7ea5a4603
Sion <simmssion@gmail.com>
2025-11-26 14:11:02 +0000

    Optimised suboptimal code
    Fixed your code Greg! :)

--- linearSearch.py
+++ linearSearch.py
@@ -1,23 +1,40 @@
-# Linear search
-nlist = [12, 43, 77, 98, 23, 72, 88, 77, 77, 77, 77]
+#26/11/25
+#Greg Allcock
+#Linear Search Program
 
-searchTerm = int(input('Enter an integer: '))
-# flag
-found = False
-count = 0
+def inputValidation():
+    while True:
+        try:
+            userInput = int(input("Enter an integer: "))
+            return userInput
+        except:
+            print("Please input an integer.")
 
-# loops through every data element in the list
-for x in range(len(nlist)):
-
-    # checks to see if the search term is in the list
-    if (searchTerm == nlist[x]):
-        # if the search term is in the list, found is assigned to True
-        found = True
-        count = count + 1
-
-# checks to see what the value of found is?
-if (found == True):
-    print('Found data item', count, 'times')
-else:
-    print('Not found data item')
+def linearSearch(nList, searchTerm):
+    count = 0
     
+    # loops through every data element in the list
+    for x in range(len(nList)):
+    
+        # checks to see if the search term is in the list
+        if (searchTerm == nList[x]):
+            count = count + 1
+            
+    # checks to see what the value of found is?
+    if (count > 0):
+        return(f'Found data item {count} times')
+
+    else:
+        return('Not found data item')
+
+def main():
+    nList = [12, 43, 77, 98, 23, 72, 88, 77, 77, 77, 77]
+
+    try:
+        print(linearSearch(nList, inputValidation()))
+        
+    except Exception as e:
+        print(f"Error of {e} type")
+
+if __name__ == "__main__":
+    main()
